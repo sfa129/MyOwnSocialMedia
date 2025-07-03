095eca6314273ccb00b17b670a19ef96ea3bd67a
SYED FAIZ AHMED <sfahmed129@gmail.com>
2025-07-03 16:23:19 +0500

    User and Video Schema Complete,

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from 'mongoose';
+import bcrypt from 'bcrypt';
+
 
 const userSchema = new mongoonse.Schema(
     {
@@ -17,7 +19,7 @@ const userSchema = new mongoonse.Schema(
             lowercase: true,
             trim: true,
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -48,5 +50,54 @@ const userSchema = new mongoonse.Schema(
         timestamps: true
     }
 );
+
+//inject the middleware into Schema i.e. "pre"
+//use this middleware to perform something before user request
+//here we are going to bcrypt the password before saving user's data
+userSchema.pre("save", async function(next){
+    //this is negative, it means that password bcrypt when password change
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+//inect the method into Schema i.e. "isPasswordCorrect"
+//check password correct or not by comparing - use compare method of bcrypt
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password) //return true or false
+}
+
+//going to make the token and inject in jwt through "sign" method of jwt
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+       { 
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+
+    ) 
+}
+
+//refresh token have few info than access token because it refresh many times
+userSchema.methods.generateRefreshToken = function () {
+return jwt.sign(
+       { 
+        _id: this._id
+    },
+    process.env.ACCESS_REFRESH_SECRET,
+    {
+        expiresIn: process.env.ACCESS_REFRESH_EXPIRY
+    }
+
+    ) 
+}
+
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,12 +1,48 @@
 import mongoose, { Schema } from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'; // used as a plugin 
+
 
 const videoSchema = new mongoose.Schema(
     {
-        
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number, //after uploading on cloudinary, it gives video information
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
     },
     {
         timestamps: true
     }
 );
+
+//before exporting it can be used
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
